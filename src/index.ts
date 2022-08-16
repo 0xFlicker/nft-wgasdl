fb48df4095853f17699892752d59c7757153e837
CmdrNFT <CmdrNFT@protonmail.com>
2022-08-16 12:46:47 -0500

    uses a resizer

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,17 @@ program
     "0x05745e72fb8b4a9b51118a168d956760e4a36444"
   )
   .option(
+    "-s, --original-size",
+    "Download in the original size rather than a minecraft skin compatible size"
+  )
+  .option(
     "-t, --token <token>",
     "Token to fetch, if not provided a random skin will be downloaded",
     Number,
     Math.floor(Math.random() * 10000)
   );
 
-program.action(async ({ contract, token, output, rpc }) => {
+program.action(async ({ contract, token, output, rpc, originalSize }) => {
   try {
     const provider = new providers.JsonRpcProvider(rpc);
     const metadataContract = IERC721Metadata__factory.connect(
@@ -36,15 +40,19 @@ program.action(async ({ contract, token, output, rpc }) => {
     const { data: metadata } = await axios.get(metadataUrl);
     // Download metadata.image to output
     let imageUrl = metadata.texture;
-    if (imageUrl.startsWith("ipfs://")) {
-      imageUrl = `https://ipfs.infura.io/ipfs/${imageUrl.substring(7)}`;
-    }
-    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+    const cid = imageUrl.substring(7);
+    const finalUrl = `https://image.0xflick.com/ipfs/${cid}?w=${
+      originalSize ? "auto" : "64"
+    }`;
+    console.log(
+      `Fetching texture (${cid}) from IPFS, this may take a while.... or fail. If it fails then try again`
+    );
+    const response = await axios.get(finalUrl, { responseType: "arraybuffer" });
     const image = response.data;
     fs.writeFileSync(join(output, `${token}.png`), image);
     console.log(`Downloaded ${token}.png`);
-  } catch (error) {
-    console.error(error);
+  } catch (error: any) {
+    console.error(error.message);
   }
 });
 
